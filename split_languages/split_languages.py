eed1d4e36c648c85a83ba917528cc134ed0323d9
vicmortelmans <vicmortelmans@gmail.com>
2019-02-04 00:14:32 +0100

    reworking

--- split_languages/split_languages.py
+++ split_languages/split_languages.py
@@ -3,7 +3,21 @@
 import sys
 from guess_language import guess_language
 
-for line in sys.stdin:
-  lang = guess_language(line, hints=['nl','la'])
-  sys.stdout.write(lang + ' ' + line)
+current_line = ''
+next_line = ''
+current_lang = 'UNKNOWN'
+next_lang = 'UNKNOWN'
 
+for line in sys.stdin:
+  previous_line = current_line
+  current_line = next_line
+  next_line = line
+  previous_lang = current_lang
+  current_lang = next_lang
+  next_lang = guess_language(next_line, hints=['nl','la'])
+  if current_lang == 'nl':
+    sys.stdout.write(current_line)
+  if current_lang == 'UNKNOWN' and (previous_lang == 'nl' or next_lang == 'nl'):
+    sys.stdout.write(current_line)
+
+# I never care about the last line of a file
